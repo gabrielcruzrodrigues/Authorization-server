f5be2e650f2610cec7f0f56e233655692bf9f3aa
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-03-19 19:01:44 -0300

    configurer basic authentication default

--- auth-server/src/main/java/br/com/gabrielrodrigues/authserver/security/AuthProperties.java
+++ auth-server/src/main/java/br/com/gabrielrodrigues/authserver/security/AuthProperties.java
@@ -2,39 +2,82 @@ package br.com.gabrielrodrigues.authserver.security;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+
 @Component
 @Validated
 @ConfigurationProperties("aw.auth")
-@Getter
-@Setter
 public class AuthProperties {
 
     @NotBlank
     private String providerUri;
 
-    @NotNull
-    private JksProperties jks;
+//    @NotNull
+//    private JksProperties jks;
 
-    @Getter
-    @Setter
-    static class JksProperties {
-
-        @NotBlank
-        private String keypass;
-
-        @NotBlank
-        private String storepass;
-
-        @NotBlank
-        private String alias;
-
-        @NotBlank
-        private String path;
+    public String getProviderUri() {
+        return providerUri;
     }
+
+    public void setProviderUri(String providerUri) {
+        this.providerUri = providerUri;
+    }
+
+//    public JksProperties getJks() {
+//        return jks;
+//    }
+
+//    public void setJks(JksProperties jks) {
+//        this.jks = jks;
+//    }
+
+//    static class JksProperties {
+//
+//        @NotBlank
+//        private String keypass;
+//
+//        @NotBlank
+//        private String storepass;
+//
+//        @NotBlank
+//        private String alias;
+//
+//        @NotBlank
+//        private String path;
+//
+//        public String getKeypass() {
+//            return keypass;
+//        }
+//
+//        public void setKeypass(String keypass) {
+//            this.keypass = keypass;
+//        }
+//
+//        public String getStorepass() {
+//            return storepass;
+//        }
+//
+//        public void setStorepass(String storepass) {
+//            this.storepass = storepass;
+//        }
+//
+//        public String getAlias() {
+//            return alias;
+//        }
+//
+//        public void setAlias(String alias) {
+//            this.alias = alias;
+//        }
+//
+//        public String getPath() {
+//            return path;
+//        }
+//
+//        public void setPath(String path) {
+//            this.path = path;
+//        }
+//    }
 }
--- auth-server/src/main/java/br/com/gabrielrodrigues/authserver/security/AuthSecurityConfig.java
+++ auth-server/src/main/java/br/com/gabrielrodrigues/authserver/security/AuthSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.gabrielrodrigues.authserver.security;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
@@ -27,9 +29,14 @@ import org.springframework.security.oauth2.server.authorization.settings.TokenSe
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.InputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -77,29 +84,36 @@ public class AuthSecurityConfig {
                 .build();
     }
 
-    //carrega as chaves publica e privada na memória
+    // assina tokens de acesso
     @Bean
-    public JWKSet jwkSet(AuthProperties authProperties) throws Exception {
-        final var jksProperties = authProperties.getJks();
-        final String jksPath = jksProperties.getPath();
-        final InputStream inputStream = new ClassPathResource(jksPath).getInputStream();
-
-        final KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(inputStream, jksProperties.getStorepass().toCharArray());
-
-        RSAKey rsaKey = RSAKey.load(keyStore, jksProperties.getAlias(), jksProperties.getKeypass().toCharArray());
-
-        return new JWKSet(rsaKey);
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
     }
 
-    //gerencia as chaves
-    @Bean
-    public JWKSource<SecurityContext> jwkSource(JWKSet jwkSet) {
-        return ((jwkSelector, securityContext) -> jwkSelector.select(jwkSet));
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        }
+        catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
     }
 
     @Bean
-    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
-        return new NimbusJwtEncoder(jwkSource);
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
+
 }
